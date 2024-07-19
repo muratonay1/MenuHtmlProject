196ccc73e32964717f6898db288152fcc71b0448
İsmet Murat ONAY <imuratony@gmail.com>
2024-07-19 21:41:59 +0300

    first commit

---
+++ Utilty/constantsVariable.js
@@ -0,0 +1,5 @@
+var PAGES={
+     "ABOUT"        : "./Pages/About/",
+     "HOME"         : "./Pages/Home/",
+     "PROJECTS"     : "./Pages/Projects/"
+}
---
+++ Utilty/index.js
@@ -0,0 +1,51 @@
+function loadContent(page) {
+     document.getElementById('content-area').innerHTML = 'Yükleniyor...';
+     $('#content-area').load(PAGES[page.getAttribute("data-page-id").toUpperCase()] + page.getAttribute("data-page-id") + ".html");
+}
+document.addEventListener("DOMContentLoaded", function () {
+     const validUsername = "admin";
+     const validPassword = "123";
+     const validVerificationCode = "123";
+
+     function loadContent(page) {
+          document.getElementById('content-area').innerHTML = 'Yükleniyor...';
+          $('#content-area').load(PAGES[page.toUpperCase()]+page+".html");
+     }
+
+     $('#loginModal').modal('show');
+
+     document.getElementById('login-form').addEventListener('submit', function (event) {
+          event.preventDefault();
+
+          const username = document.getElementById('username').value;
+          const password = document.getElementById('password').value;
+
+          if (username === validUsername && password === validPassword) {
+               document.getElementById('login-section').style.display = 'none';
+               document.getElementById('verification-section').style.display = 'block';
+          } else {
+               alert('Kullanıcı adı veya şifre hatalı!');
+          }
+     });
+
+     document.getElementById('verification-form').addEventListener('submit', function (event) {
+          event.preventDefault();
+
+          const verificationCode = document.getElementById('verification-code').value;
+
+          if (verificationCode === validVerificationCode) {
+               $('#loginModal').modal('hide');
+          } else {
+               alert('Doğrulama kodu hatalı!');
+          }
+     });
+
+     window.addEventListener('scroll', function () {
+          const navbar = document.querySelector('.navbar');
+          if (window.scrollY > 50) {
+               navbar.classList.add('scrolled');
+          } else {
+               navbar.classList.remove('scrolled');
+          }
+     });
+});
